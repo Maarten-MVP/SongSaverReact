e01ebb4551b623471d135a023eb747662ccc6ff4
Maarten Van Poucke <maarten.v.poucke@gmail.com>
2022-02-22 13:11:02 +0100

    final commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 function Navbar() {
     return (
         <div className="nav">
             <h1>Maarten's Songsaver</h1>
+            <nav className="navbar">
+                <Link to="/" >Song Saver Tool</Link>
+                <Link to="/about" >About</Link>
+            </nav>
         </div>
     )
 }
